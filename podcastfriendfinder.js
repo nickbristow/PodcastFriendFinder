79eb29d77be81e326cb021080cec589f9a23c392
Nick Bristow <Nick.Bristow@gsa.gov>
2014-10-16 11:28:15 -0400

    Some changes that dont work

--- podcastfriendfinder.js
+++ podcastfriendfinder.js
@@ -20,97 +20,9 @@ var T = new Twit({
   access_token_secret:  ''
 });
 
-var statement =   "";
-
-// insert your Wordnik API info below
-var getNounsURL = "http://api.wordnik.com/v4/words.json/randomWords?" +
-                  "minCorpusCount=1000&minDictionaryCount=10&" +
-                  "excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&" +
-                  "hasDictionaryDef=true&includePartOfSpeech=noun&limit=2&maxLength=12&" +
-                  "api_key=______YOUR_API_KEY_HERE___________";
-
-var getAdjsURL =  "http://api.wordnik.com/v4/words.json/randomWords?" +
-                  "hasDictionaryDef=true&includePartOfSpeech=adjective&limit=2&" + 
-                  "minCorpusCount=100&api_key=______YOUR_API_KEY_HERE___________";
 
 
-function makeMetaphor() {
-  statement = "";
-  restclient.get(getNounsURL,
-  function(data) {
-    first = data[0].word.substr(0,1);
-    first2 = data[1].word.substr(0,1);
-    article = "a";
-    if (first === 'a' ||
-        first === 'e' ||
-        first === 'i' ||
-        first === 'o' ||
-        first === 'u') {
-      article = "an";
-    }
-   article2 = "a";
-    if (first2 === 'a' ||
-        first2 === 'e' ||
-        first2 === 'i' ||
-        first2 === 'o' ||
-        first2 === 'u') {
-      article2 = "an";
-    }
 
-    var connector = "is";
-    switch (Math.floor(Math.random()*12)) {
-      case 0:
-        connector = "of";
-      break;
-      case 1:
-        connector = "is";
-      break;
-      case 2:
-        connector = "is";
-      break;
-      case 3:
-        connector = "considers";
-      break;
-      case 4:
-        connector = "is";
-      break;
-    }
-
-    statement += article + " " + data[0].word + " " + connector + " " + article2 + " " + data[1].word;
-
-    restclient.get(
-      getAdjsURL,
-      function(data) {
-        var connector = " and";
-        switch (Math.floor(Math.random()*8)) {
-          case 0:
-            connector = ", not";
-          break;
-          case 1:
-            connector = ", yet";
-          break;
-          case 2:
-            connector = " but";
-          break;
-          case 3:
-            connector = ",";
-          break;
-          case 4:
-            connector = ", but not";
-          break;
-        }
-        output = data[0].word + connector + " " + data[1].word;
-        statement = statement + ": " + output;
-        console.log(statement);
-        T.post('statuses/update', { status: statement}, function(err, reply) {
-          console.log("error: " + err);
-          console.log("reply: " + reply);
-        });
-      }    
-    ,"json");
-  }    
-  ,"json");
-}
 
 function favRTs () {
   T.get('statuses/retweets_of_me', {}, function (e,r) {
@@ -126,12 +38,21 @@ function favRTs () {
 // handling. it just won't tweet.
 setInterval(function() {
   try {
-    makeMetaphor();
+    T.get('search/tweets', { q: 'Game of Thrones', count: 2 }, function(err, data, response) {
+        for(var i=0;i<data.statuses.length;i++)
+        {
+           T.post('favorites/create/:id',{id: data.statuses[i].id_str},function(){});
+           console.log('favorites/create/'+data.statuses[i].id_str)
+        }
+        console.log(data)
+    });
+
+
   }
  catch (e) {
     console.log(e);
   }
-},120000);
+},20000);
 
 // every 5 hours, check for people who have RTed a metaphor, and favorite that metaphor
 setInterval(function() {
